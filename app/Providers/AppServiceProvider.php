5001b44bb12f63d2b154825e0207df7bebc0aee1
Nauris Linde <naurislinde@gmail.com>
2017-05-23 20:30:19 +0300

    Added Laravel Ide Helper

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -39,11 +39,12 @@ class AppServiceProvider extends ServiceProvider
 			 * Load third party local providers
 			 */
 		    $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
-
+		    $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
 		    /*
 			 * Load third party local aliases
 			 */
 		    $loader->alias('Debugbar', \Barryvdh\Debugbar\Facade::class);
+
 	    }
     }
 }
